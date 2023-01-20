de4507ab4e9e0354693fd781d923371388b443c0
Mali Ajay <maliajay153@gmail.com>
2023-01-20 10:48:02 +0530

    custome listview id build

--- app/src/main/java/com/example/listviewalert/MainActivity2.java
+++ app/src/main/java/com/example/listviewalert/MainActivity2.java
@@ -8,16 +8,28 @@ import android.widget.ListView;
 public class MainActivity2 extends AppCompatActivity {
     ListView costomlistview1;
     ArrayAdapter arrayAdapter;
-    String [] title = {"titiel 1","title 2","title 3","title4","title 5"};
-    String [] subtitle = {"sub title1","sub title2","sub title3","sub title4","sub title5",};
-    Integer [] imgid = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5};
+    String [] title = {"titiel 1","title 2","title 3","title4","title 5",
+            "titiel 1","title 2","title 3","title4","title 5",
+            "titiel 1","title 2","title 3","title4","title 5"};
+    String [] subtitle = {"titiel 1","title 2","title 3","title4","title 5",
+            "titiel 1","title 2","title 3","title4","title 5",
+            "titiel 1","title 2","title 3","title4","title 5"};
+    Integer [] imgid = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5,
+            R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5,
+            R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5};
+    String[] price = {"price : 200","price : 300","price : 400","price : 500","price : 1000",
+                          "price : 200","price : 300","price : 400","price : 500","price : 1000",
+                          "price : 200","price : 300","price : 400","price : 500","price : 1000"};
+    String [] brand = {"Microsonf","Alphabet","Oracle","TCS","Apple",
+                       "Microsonf","Alphabet","Oracle","TCS","Apple",
+                       "Microsonf","Alphabet","Oracle","TCS","Apple",};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        MylistView adp = new MylistView(this,title,subtitle,imgid);
+        MylistView adp = new MylistView(this,title,subtitle,imgid,price,brand);
         costomlistview1 = findViewById(R.id.customlistview1);
-        costomlistview1.setAdapter(arrayAdapter);
+        costomlistview1.setAdapter(adp);
         
 
 
--- app/src/main/java/com/example/listviewalert/MylistView.java
+++ app/src/main/java/com/example/listviewalert/MylistView.java
@@ -9,21 +9,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 public class MylistView extends ArrayAdapter<String> {
     private final Activity context;
     private final String[] title;
     private final String[] subtitle;
     private final Integer[] imgid;
+    private final String[] price;
+    private final String[] brand;
 
 
-    public MylistView(Activity context, String[] title,String[] subtitle,Integer[] imgid) {
+    public MylistView(Activity context, String[] title,String[] subtitle,Integer[] imgid,String [] price,String[] brand) {
         super(context, R.layout.mylist,title);
         this.context = context;
         this.title = title;
         this.subtitle = subtitle;
         this.imgid = imgid;
+        this.price = price;
+        this.brand = brand;
     }
 
     @NonNull
@@ -33,11 +36,16 @@ public class MylistView extends ArrayAdapter<String> {
         View rowView = inflater.inflate(R.layout.mylist,null,true);
         TextView title1 = (TextView) rowView.findViewById(R.id.txv1);
         TextView subtitle1 = (TextView)  rowView.findViewById(R.id.txv2);
+        TextView price1 = (TextView ) rowView.findViewById(R.id.mrpText);
+
+        TextView brand1 = (TextView) rowView.findViewById(R.id.brandText);
         ImageView imageView = (ImageView)  rowView.findViewById(R.id.list_item);
 
         title1.setText(title[position]);
         imageView.setImageResource(imgid[position]);
         subtitle1.setText(subtitle[position]);
+        price1.setText(price[position]);
+        brand1.setText(brand[position]);
 
         return  rowView;
     }
